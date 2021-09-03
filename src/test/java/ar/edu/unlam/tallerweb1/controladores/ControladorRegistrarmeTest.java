dd7f30805e73cd212bca4ec162cf48a699dd8df4
CobyBoy <aboutacoby@gmail.com>
2021-09-02 22:14:04 -0300

    added controller

---
+++ src/test/java/ar/edu/unlam/tallerweb1/controladores/ControladorRegistrarmeTest.java
@@ -0,0 +1,9 @@
+package ar.edu.unlam.tallerweb1.controladores;
+
+import org.junit.Test;
+
+public class ControladorRegistrarmeTest {
+    @Test
+    public void puedoRegistrarmeConUsuarioNuevoYClaveCorrecto() {
+    }
+}
